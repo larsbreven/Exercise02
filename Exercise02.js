56e167b608e6d67cd2eb022e11299eb465e9d34c
Lars Brevén <lars.breven@telia.com>
2021-03-30 21:03:10 +0200

    Stored

--- Exercise02.js
+++ Exercise02.js
@@ -12,4 +12,28 @@
     var t = date.toLocaleTimeString();
     document.getElementById("date").innerHTML = d;
     document.getElementById("time").innerHTML = t;
+
+
+  //  Exercise 2 – Leap Year Calendar
+  //  Add an input field that lets you enter a year, and tie a script to it that checks whether the year entered is 
+  //  a leap year.
+  //  Required Features:
+  //  • An input field that takes a year and calls the script when submitted. The page should not be 
+  //  refreshed.
+  //  • A script that checks whether the year entered is a leap year, and presents the result on the page, 
+  //  as a message.
+  //  Subjects:
+  //  • Reading inputs through JavaScript.
+
+
+
+
+
+
+
+
+
+
+
+
   }
